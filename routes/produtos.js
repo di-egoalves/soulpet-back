0e7c2c105623fcb61c139ae692c292c519dd2f5f
Vitor Santana <saantanavitor@gmail.com>
2023-04-27 16:23:09 -0300

    model de produtos

---
+++ routes/produtos.js
@@ -0,0 +1,8 @@
+const Cliente = require("../database/cliente");
+const Pet = require("../database/pet");
+
+const { Router } = require("express");
+
+// Criar o grupo de rotas (/produtos)
+const router = Router();
+
